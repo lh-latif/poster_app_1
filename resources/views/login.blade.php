d7fd1b01da35d4ccdd57574d52facd8de6d6dba2
Latif Hidayatullah <latifdayatofficial@gmail.com>
2020-10-23 13:49:39 +0700

    fix login and register page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,25 @@
 @extends("layouts.main")
 
 @section("body")
+  <div class="my-3">
+    <h1 class="mt-2">Login</h1>
+    <a href="/register" class="btn">
+      Register
+    </a>
+  </div>
+
   <form action="/login" method="POST">
     @csrf
-    <input type="text" name="email">
-    <input type="password" name="password">
-    <input type="submit">
+    <div class="form-group">
+      <label>Email</label>
+      <input class="form-control" type="text" name="email">
+    </div>
+
+    <div class="form-group">
+      <label>Password</label>
+      <input class="form-control" type="password" name="password">
+    </div>
+
+    <input class="btn btn-primary" type="submit" value="Login">
   </form>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,21 +1,31 @@
 @extends("layouts.main")
 
 @section("body")
-  <h1>Register</h1>
+  <div class="my-3">
+    <h1 class="mb-2">Register</h1>
+    <a href="/login" class="btn">
+      Login
+    </a>
+  </div>
+
+
   <form method="POST" action="/register">
     @csrf
-    <label>
-      Name
-    </label>
-    <input type="text" name="name" >
-    <label>
-      Email
-    </label>
-    <input type="text" name="email" >
-    <label>
-      Password
-    </label>
-    <input type="password" name="password" >
-    <input type="submit" value="register"/>
+    <div class="form-group">
+      <label>Name</label>
+      <input class="form-control" type="text" name="name" >
+    </div>
+
+    <div class="form-group">
+      <label>Email</label>
+      <input class="form-control" type="text" name="email" >
+    </div>
+
+    <div class="form-group">
+      <label>Password</label>
+      <input class="form-control" type="password" name="password" >
+    </div>
+
+    <input class="btn btn-primary" type="submit" value="Register"/>
   </form>
 @endsection
